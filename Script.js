384c1fb573944f3e3261d9bebc6d6da13b0a65b9
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-03-31 17:43:45 +0200

    j'ai ajoutee une section home

--- Script.js
+++ Script.js
@@ -17,7 +17,7 @@ async function searchWeather() {
     if (villeInput !== '') {
         const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${villeInput}&limit=5&appid=${key}`);
         const data = await response.json();
-       
+       console.log(data)
 
         if (data.length > 0) {
             const { lat, lon } = data[0];
@@ -26,8 +26,6 @@ async function searchWeather() {
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`; 
             const weatherResponse = await fetch(weatherUrl);
             const weatherData = await weatherResponse.json();
-
-             console.log(weatherData)
 
             displayWeather(weatherData);
            
@@ -45,7 +43,6 @@ async function searchWeather() {
 
 function displayWeather(data) {
    const img = getIcon(data) ; 
-   console.log(img);
     document.getElementById('imgs').src = img;
    
     const windSpeed = data.wind.speed + 'km/h'; 
@@ -185,6 +182,71 @@ function updateDate() {
 
 updateDate();
 
+
+
+// const latB = 32.334193;
+// const lonB = 6.353335;
+
+
+// async function fetchWeatherData() {
+
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latB}&lon=${lonB}&appid=${key}`);
+//     const weatherData = await response.json();
+//     const temperatureCelsiusbm = (weatherData.main.temp - 273.15).toFixed(1) + '°C';
+//     document.getElementById('temperatureBeniMellal').innerText = temperatureCelsiusbm;
+ 
+// }
+
+// fetchWeatherData();
+
+const cities = [
+    { name: 'Beni Mellal', lat: 32.334193, lon:-6.353335 },
+    { name: 'Azilal', lat: 31.959295, lon: -6.570991 },
+    { name: 'Khenifra', lat: 32.9357718, lon: -5.6696504 },
+    { name: 'Azrou', lat: 33.436117, lon: -5.221913 },
+    { name: 'Sefrou', lat: 33.824898, lon: -4.833336 },
+    { name: 'Rabat', lat: 34.022405, lon: -6.834543 },
+    { name: 'Taza', lat: 34.230155, lon: -4.010104 },
+    { name: 'Khouribga', lat: 32.885508, lon: -6.909238 },
+    { name: 'Casablanca', lat: 33.5950627, lon: -7.6187768 }
+ 
+];
+
+async function fetchWeatherData(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${key}`);
+  const weatherData = await response.json();
+  console.log(weatherData)
+
+  
+  const iconUrl = getIcon(weatherData);
+
+ 
+  const cityNameClass = city.name.replace(/\s+/g, '-').toLowerCase();
+
+
+  const images = document.querySelectorAll(`.imgs.${cityNameClass}`);
+  images.forEach(image => {
+    image.src = iconUrl;
+  });
+
+ 
+  const temperatureCelsius = (weatherData.main.temp - 273.15).toFixed(1) + '°C';
+  document.getElementById(`temperature${city.name.replace(/\s+/g, '')}`).innerText = temperatureCelsius
+ 
+}
+
+async function fetchAllWeatherData() {
+  for (const city of cities) {
+    await fetchWeatherData(city);
+  }
+}
+
+fetchAllWeatherData();
+
+document.getElementById("ser").addEventListener('click', function() {
+  document.getElementById("principal").style.display = 'flex';
+  document.getElementById("home").style.display = 'none';
+});
 
 
 
